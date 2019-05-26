705a590e43dfbe60fc72bed7bd6e72d65209f4cd
a-tree <console.0.25.0.1@gmail.com>
2019-05-26 19:45:10 +0900

    add github URL in project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cmd/lein-template "0.1.1"
   :description "Leiningen Template for Commandline apps."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/a-tree/cmdE"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
